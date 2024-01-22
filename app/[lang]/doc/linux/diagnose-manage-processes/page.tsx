35d7e5669cddcd3394128cfcebc6479f4e0e3b1c
sp <47121122+seongpil0948@users.noreply.github.com>
2024-01-23 00:00:18 +0900

    fuck

---
+++ app/[lang]/doc/linux/diagnose-manage-processes/page.tsx
@@ -0,0 +1,33 @@
+import { AVAIL_LOCALES, TAvailLocale } from '@/config/system'
+import Content from './content.mdx'
+import DocumentContainer from '~/[lang]/doc/_components/server-only/DocumentContainer'
+import { ResolvingMetadata, Metadata } from 'next'
+// import { getDictionary } from '@/app/[lang]/dictionaries'
+
+export async function generateStaticParams() {
+  return AVAIL_LOCALES.map((lang) => ({ lang }))
+}
+
+type Props = {
+  params: { lang: TAvailLocale }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata(
+  { params: { lang } }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  // const dict = await getDictionary(lang)
+  return {
+    title: 'Diagnose and manage processes',
+    description: '프로세스 진단 및 관리',
+  }
+}
+
+export default async function SSGPage({ params: { lang } }: Props) {
+  return (
+    <DocumentContainer lang={lang}>
+      <Content />
+    </DocumentContainer>
+  )
+}
