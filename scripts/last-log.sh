6bfe21a53cd031f09644201d4cf754624b881e22
최성필 <choeseongpil@choeseongpils-MacBook-Air.local>
2024-02-29 03:58:33 +0900

    doc: Exam4

--- scripts/last-log.sh
+++ scripts/last-log.sh
@@ -2,15 +2,19 @@
 # Usage: last-log.sh /logs/ixi-admin-api/svc/
 
 DIR_PATH=$1
+DEEP=$2
+
 if [ -z "$DIR_PATH" ]; then
-  # echo "Usage: last-log.sh /logs/ixi-admin-api/svc/"
-  # exit 1
   DIR_PATH="/logs/ixi-admin-api/svc"
 fi
+if [ -z "$DEEP" ]; then
+  DEEP=1
+fi
 
-ls -lt $DIR_PATH | head -n 2 | awk '{print $9}' | awk -v dir_path=$DIR_PATH '{print dir_path"/"$1}' | xargs tail -f
+if [ $DEEP -eq 1 ]; then
+  find $DIR_PATH -type f -print0 | xargs -0 stat --format '%Y :%y %n' | sort -nr | cut -d: -f2- | head -n 1 | xargs tail -f
+else
+  ls -lt $DIR_PATH | head -n 2 | awk '{print $9}' | awk -v dir_path=$DIR_PATH '{print dir_path"/"$1}' | xargs tail -f
+fi
 
-# follw the logs that last modified in the directory with depth
-# Usage: last-log.sh /logs/ixi-admin-api/svc/ 2
 
-tree -L /logs/ixi-admin-api/svc | grep -v "directories" | grep -v "files" | grep -v "bytes" | grep -v "total" | grep -v "KB" | grep -v "MB" | grep -v "GB" | grep -v "TB" | grep -v "B" | grep -v "K" | grep -v "M" | grep -v "G" | grep -v "T
